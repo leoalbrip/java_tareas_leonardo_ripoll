7e71fcc1dd0e2d1f6fb7868619492a6a9224459a
leoalbrip <leoalbrip@gmail.com>
2023-02-28 09:44:05 -0300

    quinto commit

--- assets/js/past_events.js
+++ assets/js/past_events.js
@@ -1,18 +1,30 @@
-let contenedor = document.getElementById('id_card');
+let fecha = data.currentDate;
+let nuevaFecha = new Date (fecha);
+let arrayAux =[] ;
+let fechaPast;
 
-for (let tarjeta of data.events) {
+for(let tarjeta_past of data.events){
+     if(fechaPast= new Date(tarjeta_past.date) < nuevaFecha){
+           arrayAux.push(tarjeta_past)
+      }
+ };
+
+ console.log(arrayAux);
+
+ let contenedor = document.getElementById('id_card');
+ for (tarjeta_id of arrayAux) {
     let div = document.createElement('div');
     div.classList.add('card');
-    div.innerHTML = `<img src='${tarjeta.image}' class="card-img-top" alt="'${tarjeta.name}'"></img>
+    div.innerHTML = `<img src='${tarjeta_id.image}' class="card-img-top" alt="'${tarjeta_id.name}'"></img>
     <div class="card-body">
-                     <h5 class="card-title">${tarjeta.name}</h5>
-                      <p class="card-text">Date: ${tarjeta.date}</p>
-                      <p class="card-text">Description: ${tarjeta.description}</p>
-                      <p class="card-text">Category: ${tarjeta.category}</p>
-                      <p class="card-text">Place: ${tarjeta.place}</p>
-                      <p class="card-text">Capacity: ${tarjeta.capacity}</p>
-                      <p class="card-text">Assistance: ${tarjeta.assistance}</p>
-                      <p class="card-text">Price: ${tarjeta.price}</p>
+                     <h5 class="card-title">${tarjeta_id.name}</h5>
+                      <p class="card-text">Date: ${tarjeta_id.date}</p>
+                      <p class="card-text">Description: ${tarjeta_id.description}</p>
+                      <p class="card-text">Category: ${tarjeta_id.category}</p>
+                      <p class="card-text">Place: ${tarjeta_id.place}</p>
+                      <p class="card-text">Capacity: ${tarjeta_id.capacity}</p>
+                      <p class="card-text">Assistance: ${tarjeta_id.assistance}</p>
+                      <p class="card-text">Price: ${tarjeta_id.price}</p>
 
                       <a href="./details.html" class="btn btn-primary">Details</a>
                       </div>`
